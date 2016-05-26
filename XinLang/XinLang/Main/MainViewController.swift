88eacdbb03ab4a2ab867dafac2136b827ad8f02c
cqz930110 <1024463782@qq.com>
2016-05-26 22:52:59 +0800

    新建项目

---
+++ XinLang/XinLang/Main/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  XinLang
+//
+//  Created by cqz on 16/5/26.
+//  Copyright © 2016年 cqz. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
